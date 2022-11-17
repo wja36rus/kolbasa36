8a41397134844040ddbff38e203308fe500681a4
wja36rus <48429607+wja36rus@users.noreply.github.com>
2022-11-17 23:56:45 +0300

    add form

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    new Splide( '#image-carousel', {
+    new Splide('#image-carousel', {
         type: 'loop',
-        fixedWidth : '20rem',
+        fixedWidth: '20rem',
         fixedHeight: '20rem',
-        gap        : '1rem',
-        perPage:4,
-        perMove:1,
-    } ).mount();
+        gap: '1rem',
+        perPage: 4,
+        perMove: 1,
+    }).mount();
 
     var $grid = $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: '.sizer4'
-      }
+            itemSelector: '.grid-item',
+            columnWidth: '.sizer4'
+        }
     );
-    
-    $grid.imagesLoaded().progress( function() {
+
+    $grid.imagesLoaded().progress(function () {
         $grid.masonry('layout');
         console.log('loaded');
     });
@@ -30,58 +30,99 @@ $(document).ready(function(){
             mob_menu.addClass('active').removeClass('notactive');
             burger_state = 'active';
             console.log('active');
-        }
-        else if (burger_state == 'active') {
+        } else if (burger_state == 'active') {
             burger_button.addClass('notactive').removeClass('active');
             mob_menu.addClass('notactive').removeClass('active');
             burger_state = 'notactive'
             console.log('notactive');
-        };
+        }
+        ;
     }
 
     $('.mob-nav a').on('click', menu_open)
     burger_button.on('click', menu_open);
 
     var Modal = $('#ModalImage');
-   
+
     var img = $(".grid-item > img");
     var ModalIgm = $(".modal-image-content");
     var ModalClose = $(".modal-close");
 
 
-
-    img.on('click', function(){
+    img.on('click', function () {
         Modal.css("display", "flex");
         ModalIgm.attr("src", this.src);
         console.log(this.src);
     });
 
-    ModalClose.on('click', function(){
+    ModalClose.on('click', function () {
         Modal.css("display", "none");
     })
 
-    var form = $('.form'),
-      phone_number = $('#message'),
-      ModalSent = $('.modal-sent'),
-      ModalSentClose = $('.modal-sent .button'),
-      submit = $(".submit");
-
-
-    submit.on('click', function(e){
+    $('#send1').click(function (e) {
         e.preventDefault();
-        var data = form.serialize();
-        $.ajax({
-            type: "POST",
-            url: "send_mail.php",
-            data: form.serialize(),
-            dataType: "json"
-          }).done(function(data) {
-            form[0].reset();
-            ModalSent.css("display", "flex")
-            ModalSentClose.on("click", function(){
-            ModalSent.css("display", "none");
-         });
- 
-         })
-    })
+        var SenderName = $('#SenderName');
+        var SenderPhone = $('#SenderPhone');
+
+        if (SenderName.val() === "") {
+            $('.error1').empty().text("Введите имя");
+        } else if (SenderPhone.val() === "") {
+            $('.error1').empty().text("Введите телефон");
+        } else {
+            $('.error1').empty();
+            $('.success1').removeClass('d-none');
+
+            setTimeout(() => {
+                $('.success1').addClass('d-none');
+                SenderName.val('');
+                SenderPhone.val('');
+            }, 1500)
+        }
+
+    });
+
+    $('#send2').click(function (e) {
+        e.preventDefault();
+        var SenderName = $('#SenderName2');
+        var SenderPhone = $('#SenderPhone2');
+
+        if (SenderName.val() === "") {
+            $('.error2').empty().text("Введите имя");
+        } else if (SenderPhone.val() === "") {
+            $('.error2').empty().text("Введите телефон");
+        } else {
+            $('.error2').empty();
+            $('.success2').removeClass('d-none');
+
+            setTimeout(() => {
+                $('.success2').addClass('d-none');
+                SenderName.val('');
+                SenderPhone.val('');
+            }, 1500)
+        }
+
+    });
+
+    $('#send3').click(function (e) {
+        e.preventDefault();
+        var SenderName = $('#SenderName3');
+        var SenderPhone = $('#SenderPhone3');
+
+        if (SenderName.val() === "") {
+            $('.error3').empty().text("Введите имя");
+        } else if (SenderPhone.val() === "") {
+            $('.error3').empty().text("Введите телефон");
+        } else {
+            $('.error3').empty();
+            $('.success3').removeClass('d-none');
+
+            setTimeout(() => {
+                $('.success3').addClass('d-none');
+                SenderName.val('');
+                SenderPhone.val('');
+            }, 1500)
+        }
+
+    });
+
 });
